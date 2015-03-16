c4376bc7251ce18dd6b2b8dccd9ba41739baa4b4
larilampen <lari.lampen@gmail.com>
2015-03-16 13:47:34 +0100

    Starting point: a new Blacklight application (with jettywrapper but without devise or marc) with no changes.

---
+++ config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+# Blacklight.secret_key = '3e197e4b10060aaaddd1320eeee3691f120c5d6b79c34cd24e13ecab3f9362c04c79de9e629ad89b99f8ce7d43b3d6d78f853f22ee5c29ed25a767dc4fc98d9d'
+
