55abd6795053f3f5b5dc60b6a599b658cf162740
larilampen <lari.lampen@gmail.com>
2015-03-19 23:59:13 +0100

    Set locale based on browser's Accept-Language header. Added a couple of translations as a starting point.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,22 @@ class ApplicationController < ActionController::Base
 
   layout 'blacklight'
 
+  before_action :set_locale
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def set_locale
+    logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+    I18n.locale = extract_locale_from_accept_language_header
+    logger.debug "* Locale set to '#{I18n.locale}'"
+  end
+
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/) do |str|
+      return str if I18n.available_locales.include? str.to_sym
+    end
+    return I18n.default_locale
+  end
 end
