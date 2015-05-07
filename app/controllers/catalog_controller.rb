967b7aeb68ded39ef69e9279bbb693b3c6c9fb79
larilampen <lari.lampen@gmail.com>
2015-05-07 14:14:21 +0200

    Trailing whitespace removed, no other change.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 #
-class CatalogController < ApplicationController  
+class CatalogController < ApplicationController
 
   include Blacklight::Catalog
 
@@ -8,19 +8,19 @@ class CatalogController < ApplicationController
 
   configure_blacklight do |config|
     ## Default parameters to send to solr for all search-like requests. See also SolrHelper#solr_search_params
-    config.default_solr_params = { 
+    config.default_solr_params = {
       :qf => 'text',
       :qt => 'search',
-      :rows => 10 
+      :rows => 10
     }
-    
+
     # solr path which will be added to solr base url before the other solr params.
-    #config.solr_path = 'select' 
-    
+    #config.solr_path = 'select'
+
     # items to show per page, each number in the array represent another option to choose from.
     #config.per_page = [10,20,50,100]
 
-    ## Default parameters to send on single-document requests to Solr. These settings are the Blackligt defaults (see SolrHelper#solr_doc_params) or 
+    ## Default parameters to send on single-document requests to Solr. These settings are the Blackligt defaults (see SolrHelper#solr_doc_params) or
     ## parameters included in the Blacklight-jetty document requestHandler.
     #
     #config.default_document_solr_params = {
@@ -28,7 +28,7 @@ class CatalogController < ApplicationController
     #  ## These are hard-coded in the blacklight 'document' requestHandler
     #  # :fl => '*',
     #  # :rows => 1
-    #  # :q => '{!raw f=id v=$id}' 
+    #  # :q => '{!raw f=id v=$id}'
     #}
 
     # solr field configuration for search results/index views
@@ -61,17 +61,17 @@ class CatalogController < ApplicationController
     # previously. Simply remove these lines if you'd rather use Solr request
     # handler defaults, or have no facets.
     config.add_facet_fields_to_solr_request!
-    
-    
+
+
     # "sort results by" select (pulldown)
     # label in pulldown is followed by the name of the SOLR field to sort by and
     # whether the sort is ascending or descending (it must be asc or desc
     # except in the relevancy case).
     # (TO BE ADDED)
 
-    # If there are more than this many search results, no spelling ("did you 
+    # If there are more than this many search results, no spelling ("did you
     # mean") suggestion is offered.
     config.spell_max = 5
   end
 
-end 
+end
