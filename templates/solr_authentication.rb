affcbfcf3a9e82bf095e093b8850d23f88271721
larilampen <lari.lampen@gmail.com>
2015-06-25 15:44:14 +0200

    Changed the default Solr user to 'orfeo' from 'admin', since it is used for all requests and not specifically for administration.

--- templates/solr_authentication.rb
+++ templates/solr_authentication.rb
@@ -6,7 +6,7 @@ class RSolr::Connection
   alias :old_setup_raw_request :setup_raw_request
   def setup_raw_request request_context
     raw_request = old_setup_raw_request request_context
-    raw_request.basic_auth('admin', 'SOLR_PASSWORD');
+    raw_request.basic_auth('orfeo', 'SOLR_PASSWORD');
     raw_request
   end
 end
