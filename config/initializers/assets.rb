de4a6079ea9097d4d361012534fd5dbf77271d42
larilampen <lari.lampen@gmail.com>
2015-07-25 15:24:09 +0800

    Path handling changes to avoid breaking asset pipeline when app is installed in a subdirectory (e.g. /search).

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,10 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+if ENV['ORFEO_SEARCH_ROOT']
+  Rails.application.config.action_controller.relative_url_root = ENV['ORFEO_SEARCH_ROOT']
+end
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
