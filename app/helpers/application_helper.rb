c950aca144b61e0db13401987c111bad4add6d01
larilampen <lari.lampen@gmail.com>
2015-09-15 22:48:06 +0200

    Concordancer: string splitter merges contiguous matches. If a phrase is specified, it throws away all matches that differ from it. The concordancer view uses this feature if (and only if) the search term is in quotes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,13 +26,15 @@ module ApplicationHelper
 
   # Split string into left and right context around each match.
   # The context sizes represent number of words.
+  # If phrase is specified, only matching results are shown.
   # Yields left context, match, right context, start and end indices of the match.
-  def split_context(str, lc_size = 5, rc_size = 5)
+  def split_context(str, lc_size = 5, rc_size = 5, phrase = nil)
     return unless block_given?
 
     # Split string into words, then find which words are matches. It is assumed
     # that the start and end tags are balanced, and that there is at most one
-    # mark tag within a word.
+    # mark tag within a word. We store a list of indices (of words) where
+    # highlighted strings start and another list of indices where they end.
     words = str.split
     starts = []
     ends = []
@@ -47,6 +49,51 @@ module ApplicationHelper
         ends.push idx
         word.sub!(/<\/mark>/, '')
         in_match = false
+      end
+    end
+
+    # After this, phrase is either nil or an array of words.
+    phrase = phrase.split if phrase
+
+    # Merge contiguous matches.
+    i = 0
+    while i < starts.size
+      # Start from index i and loop j upwards, checking whether they form a
+      # contiguous set of matching words to be combined.
+      # Match status is always either :fail, :ok or blank (nil).
+      j = i
+      match_status = nil
+      while j < starts.size && (i==j || starts[j] == ends[j-1]+1)
+        if phrase && words[starts[j]] != phrase[j-i]
+          match_status = :fail
+          break
+        end
+        j += 1
+        if phrase && j-i == phrase.size
+          # The match is complete.
+          match_status = :ok
+          break
+        end
+      end
+
+      # If the match is not complete at the end of the string (e.g. searching
+      # for "bien que" but the last word of the string is "bien"), reject it.
+      if phrase && match_status.nil? && j-i < phrase.size
+        match_status = :fail
+      end
+
+      if match_status == :fail
+        # Just remove the first non-matching word before trying again.
+        starts.slice!(i)
+        ends.slice!(i)
+      else
+        j -= 1
+        if j > i
+          ends[i] = ends[j]
+          starts.slice!(i+1..j)
+          ends.slice!(i+1..j)
+        end
+        i += 1
       end
     end
 
